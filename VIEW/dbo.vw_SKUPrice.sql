795854101f76af1f145726b0e12ed62d2885f20b
Denis Shuvalov <127241462+Py-destroyer@users.noreply.github.com>
2024-03-22 15:01:32 +0100

    Update dbo.vw_SKUPrice.sql

---
+++ VIEW/dbo.vw_SKUPrice.sql
@@ -0,0 +1,11 @@
+use database;
+
+if object_id('dbo.vw_SKUPrice') is null
+begin
+    create view dbo.vw_SKUPrice 
+    as
+        select 
+            SKU.*
+            ,dbo.udf_GetSKUPrice(SKU.ID) as Price
+        from dbo.SKU;
+end
